3ccc76b3269f73f46a042d3a76b5ebe4f45791a7
tony-xlh <tony@dynamsoft.com>
2025-07-10 16:18:54 +0800

    use local resources for angular

--- angular/src/app/components/viewer/viewer.component.ts
+++ angular/src/app/components/viewer/viewer.component.ts
@@ -20,7 +20,7 @@ export class ViewerComponent {
     })
 
     DDV.Core.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAwMjI3NzYzLU1UQXdNakkzTnpZekxYZGxZaTFVY21saGJGQnliMm8iLCJtYWluU2VydmVyVVJMIjoiaHR0cHM6Ly9tbHRzLmR5bmFtc29mdC5jb20iLCJvcmdhbml6YXRpb25JRCI6IjEwMDIyNzc2MyIsInN0YW5kYnlTZXJ2ZXJVUkwiOiJodHRwczovL3NsdHMuZHluYW1zb2Z0LmNvbSIsImNoZWNrQ29kZSI6LTE2NDMyMjI1ODl9';
-    DDV.Core.engineResourcePath = 'https://cdn.jsdelivr.net/npm/dynamsoft-document-viewer@latest/dist/engine';
+    DDV.Core.engineResourcePath = '/lib/dynamsoft-document-viewer/engine';
     DDV.Core.loadWasm();
     await DDV.Core.init();
 
